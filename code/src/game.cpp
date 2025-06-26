7b08be38edbc4463be14f12b271047ba21653672
WintermelonC <145361960+WintermelonC@users.noreply.github.com>
2025-06-26 15:09:24 +0800

    feat: add water texture background
    - the resizing of the window may need to be adjusted later

--- code/src/game.cpp
+++ code/src/game.cpp
@@ -3,9 +3,18 @@
 Game::Game()
     : m_window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Surf Game"),
       m_view(sf::FloatRect({0.f, 0.f}, {WINDOW_WIDTH, WINDOW_HEIGHT})),
-      m_isRunning(true) {
+      m_isRunning(true),
+      m_backgroundTexture(m_backgroundPath),
+      m_background(m_backgroundTexture) {
     
     m_window.setView(m_view);
+    // 激活垂直同步
+    m_window.setVerticalSyncEnabled(true);
+
+    // 纹理重复平铺
+    m_backgroundTexture.setRepeated(true);
+    // 设置精灵纹理矩形
+    m_background.setTextureRect(sf::IntRect({0, 0}, {RENDER_WIDTH, RENDER_HEIGHT}));
 }
 
 void Game::run() {
@@ -19,7 +28,12 @@ void Game::run() {
 void Game::handleEvents() {
     while (const std::optional event = m_window.pollEvent()) {
         if (event -> is<sf::Event::Closed>()) {
+            // 关闭窗口事件
             m_window.close();
+        } else if (const auto* resized = event -> getIf<sf::Event::Resized>()) {
+            // 窗口大小调整事件
+            sf::FloatRect visibleArea({0.f, 0.f}, sf::Vector2f(resized -> size));
+            m_window.setView(sf::View(visibleArea));
         }
     }
 }
@@ -29,6 +43,7 @@ void Game::update() {
 }
 
 void Game::render() {
-    m_window.clear(sf::Color(0, 192, 222));
-    m_window.display();
+    m_window.clear(sf::Color(0, 192, 222));  // 用纯色清除窗口
+    m_window.draw(m_background);  // 绘制背景精灵
+    m_window.display();  // 显示渲染结果
 }
--- code/src/include/game.h
+++ code/src/include/game.h
@@ -7,11 +7,14 @@
 
 #pragma once
 
+#include <string>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
-constexpr int WINDOW_WIDTH = 1280;  // 窗口宽度
-constexpr int WINDOW_HEIGHT = 1440;  // 窗口高度
+constexpr int RENDER_WIDTH = 2560;  // 渲染宽度
+constexpr int RENDER_HEIGHT = 1440;  // 渲染高度
+constexpr int WINDOW_WIDTH = 540;  // 窗口宽度
+constexpr int WINDOW_HEIGHT = 960;  // 窗口高度
 
 class Game {
 public:
@@ -32,7 +35,10 @@ private:
     void render();  
 
 private:
-    sf::RenderWindow m_window;
-    sf::View m_view;
+    sf::RenderWindow m_window;  // 窗口
+    sf::View m_view;  // 视图
+    std::string m_backgroundPath{"../../assets/images/water_texture.png"};
+    sf::Texture m_backgroundTexture;  // 背景纹理
+    sf::Sprite m_background;  // 背景精灵
     bool m_isRunning;
 };
