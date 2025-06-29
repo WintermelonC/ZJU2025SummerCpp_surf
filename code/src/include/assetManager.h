e43a97eb25c472fc423be3b3817bfa73b1512880
KappaFish <1523043877@qq.com>
2025-06-29 14:51:33 +0800

    feat: finish start/playing/pause Gamestates

--- code/src/include/assetManager.h
+++ code/src/include/assetManager.h
@@ -5,17 +5,34 @@
 #include <SFML/Graphics.hpp>
 
 enum class Textures {
-    water
+    water,
+    start_button,
+    start_icon,
+    scoreboard,
+    player_center_1, player_center_2, player_center_3,
+    player_left_11, player_left_12, player_left_13,
+    player_left_21, player_left_22, player_left_23,
+    player_right_11, player_right_12, player_right_13,
+    player_right_21, player_right_22, player_right_23,
+    player_stop_1, player_stop_2, player_stop_3,
+    heart_1, heart_2, power_1, power_2,
+};
+
+enum class Fonts {
+    MSYHBD,
 };
 
 class AssetManager {
 public:
     static void loadAssets();
     static sf::Texture& getTexture(const Textures& texture);
+    static sf::Font& getFont(const Fonts& font);
 
 private:
     static void loadTexture(const Textures& texture, const std::string& filePath);
+    static void loadFont(const Fonts& font, const std::string& filePath);
 
 private:
     static std::map<Textures, sf::Texture> m_textures;  // 存储纹理
+    static std::map<Fonts, sf::Font> m_fonts;  // 存储字体
 };
--- code/src/include/game.h
+++ code/src/include/game.h
@@ -7,6 +7,13 @@
 #include "inputManager.h"
 #include "renderSystem.h"
 
+enum class GameState {
+    StartMenu,
+    Playing,
+    Paused,
+    GameOver
+};
+
 class Game {
 public:
     Game();
@@ -22,6 +29,7 @@ private:
     sf::RenderWindow m_window;  // 窗口
     sf::View m_view;  // 视图
     sf::Clock m_clock;  // 时钟
+    GameState m_gameState;  // 游戏状态
 
     EventBus m_eventBus;  // 事件总线
     InputManager m_inputManager;  // 输入管理器
