4e5efee86919959b80725f0bd5a76f48181dfc47
WintermelonC <145361960+WintermelonC@users.noreply.github.com>
2025-07-06 16:32:03 +0800

    feat: add new obstacle images and update obstacle patterns

--- code/src/common/textureManager.cpp
+++ code/src/common/textureManager.cpp
@@ -35,10 +35,12 @@ bool TextureManager::initialize() {
     success &= loadTexture(TextureType::player_stop_1, playerPath + "player_stop_1.png");
     success &= loadTexture(TextureType::player_stop_2, playerPath + "player_stop_2.png");
     success &= loadTexture(TextureType::player_stop_3, playerPath + "player_stop_3.png");
+
     success &= loadTexture(TextureType::heart_1, playerPath + "heart_1.png");
     success &= loadTexture(TextureType::heart_2, playerPath + "heart_2.png");
     success &= loadTexture(TextureType::power_1, playerPath + "power_1.png");
     success &= loadTexture(TextureType::power_2, playerPath + "power_2.png");
+
     success &= loadTexture(TextureType::wood_1, obstaclePath + "wood_1.png");
     success &= loadTexture(TextureType::wood_2, obstaclePath + "wood_2.png");
     success &= loadTexture(TextureType::wood_3, obstaclePath + "wood_3.png");
@@ -58,7 +60,36 @@ bool TextureManager::initialize() {
     success &= loadTexture(TextureType::seaweed_11, obstaclePath + "seaweed_11.png");
     success &= loadTexture(TextureType::seaweed_21, obstaclePath + "seaweed_21.png");
     success &= loadTexture(TextureType::seaweed_31, obstaclePath + "seaweed_31.png");
+    success &= loadTexture(TextureType::ripple_11, obstaclePath + "ripple_11.png");
+    success &= loadTexture(TextureType::ripple_21, obstaclePath + "ripple_21.png");
+    success &= loadTexture(TextureType::ripple_31, obstaclePath + "ripple_31.png");
     success &= loadTexture(TextureType::bridge_1, obstaclePath + "bridge_1.png");
+    success &= loadTexture(TextureType::sp_bridge_1, obstaclePath + "sp_bridge_1.png");
+    success &= loadTexture(TextureType::buoy_1, obstaclePath + "buoy_1.png");
+    success &= loadTexture(TextureType::buoy_2, obstaclePath + "buoy_2.png");
+    success &= loadTexture(TextureType::coral_1, obstaclePath + "coral_1.png");
+    success &= loadTexture(TextureType::coral_2, obstaclePath + "coral_2.png");
+    success &= loadTexture(TextureType::coral_3, obstaclePath + "coral_3.png");
+    success &= loadTexture(TextureType::coral_4, obstaclePath + "coral_4.png");
+    success &= loadTexture(TextureType::l_beach_1, obstaclePath + "l_beach_1.png");
+    success &= loadTexture(TextureType::l_beach_2, obstaclePath + "l_beach_2.png");
+    success &= loadTexture(TextureType::l_beach_3, obstaclePath + "l_beach_3.png");
+    success &= loadTexture(TextureType::l_bridge_1, obstaclePath + "l_bridge_1.png");
+    success &= loadTexture(TextureType::l_bridge_2, obstaclePath + "l_bridge_2.png");
+    success &= loadTexture(TextureType::m_beach_1, obstaclePath + "m_beach_1.png");
+    success &= loadTexture(TextureType::m_beach_2, obstaclePath + "m_beach_2.png");
+    success &= loadTexture(TextureType::m_beach_3, obstaclePath + "m_beach_3.png");
+    success &= loadTexture(TextureType::m_beach_4, obstaclePath + "m_beach_4.png");
+    success &= loadTexture(TextureType::m_beach_5, obstaclePath + "m_beach_5.png");
+    success &= loadTexture(TextureType::m_boat_1, obstaclePath + "m_boat_1.png");
+    success &= loadTexture(TextureType::m_bridge_1, obstaclePath + "m_bridge_1.png");
+    success &= loadTexture(TextureType::m_bridge_2, obstaclePath + "m_bridge_2.png");
+    success &= loadTexture(TextureType::mm_bridge_1, obstaclePath + "mm_bridge_1.png");
+    success &= loadTexture(TextureType::mm_beach_1, obstaclePath + "mm_beach_1.png");
+    success &= loadTexture(TextureType::mm_beach_2, obstaclePath + "mm_beach_2.png");
+    success &= loadTexture(TextureType::mm_beach_3, obstaclePath + "mm_beach_3.png");
+    success &= loadTexture(TextureType::s_buoy_1, obstaclePath + "s_buoy_1.png");
+    success &= loadTexture(TextureType::s_buoy_2, obstaclePath + "s_buoy_2.png");
 
     return success;
 }
--- code/src/models/obstacleModel.h
+++ code/src/models/obstacleModel.h
@@ -29,8 +29,68 @@ enum class Seaweed {
     count
 };
 
+enum class Ripple {
+    ripple_11, ripple_21, ripple_31,
+    count
+};
+
 enum class Bridge {
     bridge_1,
+    count
+};
+
+enum class SpBridge {
+    sp_bridge_1,
+    count
+};
+
+enum class Buoy {
+    buoy_1, buoy_2,
+    count
+};
+
+enum class Coral {
+    coral_1, coral_2, coral_3, coral_4,
+    count
+};
+
+enum class LBeach {
+    l_beach_1, l_beach_2, l_beach_3,
+    count
+};
+
+enum class LBridge {
+    l_bridge_1, l_bridge_2,
+    count
+};
+
+enum class MBeach {
+    m_beach_1, m_beach_2, m_beach_3, m_beach_4, m_beach_5,
+    count
+};
+
+enum class MBoat {
+    m_boat_1,
+    count
+};
+
+enum class MBridge {
+    m_bridge_1, m_bridge_2,
+    count
+};
+
+enum class MMBridge {
+    mm_bridge_1,
+    count
+};
+
+enum class MMBeach {
+    mm_beach_1, mm_beach_2, mm_beach_3,
+    count
+};
+
+enum class SBuoy {
+    s_buoy_1, s_buoy_2,
     count
 };
 
@@ -42,7 +102,19 @@ enum class ObstacleType {
     boat,
     beach,
     seaweed,
+    ripple,
     bridge,
+    sp_bridge,
+    buoy,
+    coral,
+    l_beach,
+    l_bridge,
+    m_beach,
+    m_boat,
+    m_bridge,
+    mm_bridge,
+    mm_beach,
+    s_buoy,
     any  // 表示可以是任意类型
 };
 
@@ -58,9 +130,26 @@ public:
     static int getBeachCount() { return static_cast<int>(Beach::count); }
     static int getSeaweedCount() { return static_cast<int>(Seaweed::count); }
     static int getBridgeCount() { return static_cast<int>(Bridge::count); }
+    static int getRippleCount() { return static_cast<int>(Ripple::count); }
+    static int getSpBridgeCount() { return static_cast<int>(SpBridge::count); }
+    static int getBuoyCount() { return static_cast<int>(Buoy::count); }
+    static int getCoralCount() { return static_cast<int>(Coral::count); }
+    static int getLBeachCount() { return static_cast<int>(LBeach::count); }
+    static int getLBridgeCount() { return static_cast<int>(LBridge::count); }
+    static int getMBeachCount() { return static_cast<int>(MBeach::count); }
+    static int getMBoatCount() { return static_cast<int>(MBoat::count); }
+    static int getMBridgeCount() { return static_cast<int>(MBridge::count); }
+    static int getMMBridgeCount() { return static_cast<int>(MMBridge::count); }
+    static int getMMBeachCount() { return static_cast<int>(MMBeach::count); }
+    static int getSBuoyCount() { return static_cast<int>(SBuoy::count); }
+    
     static int getObstacleCount() { 
         return getWoodCount() + getStoneCount() + getBoatCount() + 
-               getBeachCount() + getSeaweedCount() + getBridgeCount(); 
+               getBeachCount() + getSeaweedCount() + getBridgeCount() +
+               getRippleCount() + getSpBridgeCount() + getBuoyCount() + 
+               getCoralCount() + getLBeachCount() + getLBridgeCount() + 
+               getMBeachCount() + getMBoatCount() + getMBridgeCount() +
+               getMMBridgeCount() + getMMBeachCount() + getSBuoyCount();
     }
 
     // Getter 方法
